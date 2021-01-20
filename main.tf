763409a53074cd4b680abcd86a8beaeee525fe6a
psadagop <praba95014@gmail.com>
2021-01-20 21:13:23 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -93,6 +93,7 @@ resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
   location            = var.location
   Department          = "devops"
+  Billable            = "true"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
 
