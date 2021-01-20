8938ea7c9f9ba25d6be0e050b1ee42064d0970c4
psadagop <praba95014@gmail.com>
2021-01-20 21:33:17 +0000

    Added tags right

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+
 }
 
 # We're using a little trick here so we can run the provisioner without
