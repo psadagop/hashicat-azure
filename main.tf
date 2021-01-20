6e6ab42749ee011689a4b2e3dab3a2fab7913043
psadagop <praba95014@gmail.com>
2021-01-20 21:10:50 +0000

    added the first tag

--- main.tf
+++ main.tf
@@ -92,6 +92,7 @@ resource "azurerm_public_ip" "catapp-pip" {
 resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
   location            = var.location
+  Department          = "devops"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
 
